ff9ed3eeb8f3717f0808202bf3a4b6360f6d680c
Philippe Tellier <philippe@activix.ca>
2016-05-02 22:27:02 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Vue.js / PHP / Bootstrap boilerplate
+
+## Includes
+> Vue.js
+> Bootstrap
+> SASS
+> Command to run default Mac's PHP server
+
+## Build Setup
+
+``` bash
+# Install dependencies (Restart of terminal is required after that)
+npm install
+
+# Serve with hot reload at localhost:8080
+npm run dev
+
+# Build for production with minification
+npm run build
+```
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Vue.JS / PHP / Bootstrap boilerplate</title>
+
+	<!-- Stylesheets -->
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h1>Hello World</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Scripts -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
